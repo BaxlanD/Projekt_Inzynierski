899d23ca963e79141f2481683b6da082f39b73c6
DawidKrolikiewicz <dawid.krolikiewicz@student.up.krakow.pl>
2026-01-19 20:24:56 +0100

    light fix

--- levels/level-village/level_village.gd
+++ levels/level-village/level_village.gd
@@ -9,10 +9,13 @@ func set_active_layer(layer_index: int) -> void:
 	for i in navigator.get_child_count():
 		var layer : LevelLayer = navigator.get_child(i)
 		layer.modulate.a = 1.0 if i == layer_index else 0.0
+		layer.visible = i == layer_index
 	for i in npcs.get_child_count():
 		var npc : NPCActions = npcs.get_child(i)
 		var npc_agent : AnchoredAgentV2 = npc.get_node("AnchoredAgentV2")
 		npc.modulate.a = 1.0 if npc_agent.actor_layer == layer_index else 0.0
+		npc.visible = i == layer_index
 	for i in items.get_child_count():
 		var item : Item = items.get_child(i)
 		item.modulate.a = 1.0 if item.actor_layer == layer_index else 0.0
+		item.visible = i == layer_index
