86b53f6e576cd95eae77497ca2f726afe64080a9
DawidKrolikiewicz <dawid.krolikiewicz@student.up.krakow.pl>
2026-01-20 21:34:10 +0100

    add reset button - wow thats crazy

--- levels/level-village/level_village.gd
+++ levels/level-village/level_village.gd
@@ -5,6 +5,14 @@ class_name Level
 @onready var npcs: Node = $NPCs
 @onready var items: Node = $Items
 
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("reset"):
+		queue_free()
+		reset.call_deferred()
+
+func reset() -> void:
+	SceneTransition.change_scene("res://levels/level-village/level_village.tscn")
+
 func set_active_layer(layer_index: int) -> void:
 	for i in navigator.get_child_count():
 		var layer : LevelLayer = navigator.get_child(i)
